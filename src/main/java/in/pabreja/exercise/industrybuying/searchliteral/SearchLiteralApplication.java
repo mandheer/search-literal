1011d2f080e53a9c1bca473d774946e8318b65ae
Mandheer Pabreja <mandheer@pabreja.in>
2019-04-19 12:12:35 +0530

    initial boot

---
+++ src/main/java/in/pabreja/exercise/industrybuying/searchliteral/SearchLiteralApplication.java
@@ -0,0 +1,13 @@
+package in.pabreja.exercise.industrybuying.searchliteral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchLiteralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchLiteralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/pabreja/exercise/industrybuying/searchliteral/SearchLiteralApplicationTests.java
@@ -0,0 +1,16 @@
+package in.pabreja.exercise.industrybuying.searchliteral;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchLiteralApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
